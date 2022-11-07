d4e39e0e2d4d3dcf60ad34667cbe6ac4ed474672
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-11-06 17:19:04 -0700

    edited server.js to use handlebars as template engine, created homepage view, styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,9 +8,16 @@ router.get("/", async (req, res) => {
     });
 
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    // const comments = blogs.map((comment) => {
+    //   return comment.comments;
+    // });
+    // console.log(comments);
+    // console.log(blogs);
     // res.status(200).json(blogData);
-    res.render("homepage", {
+
+    res.render('homepage', {
       blogs,
+      // comments,
     });
   } catch (err) {
     res.status(500).json(err);
