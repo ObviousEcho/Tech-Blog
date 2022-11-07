b1b2365132cb618d9215393b39cedaa0e8405d71
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-11-06 20:51:27 -0700

    login in and logout functions working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+})
+
 router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -47,6 +56,7 @@ router.post("/logout", (req, res) => {
     req.session.destroy(() => {
       res.status(204).end();
     });
+  } else {
     res.status(404).end();
   }
 });
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,10 +14,19 @@ router.get("/", async (req, res) => {
 
     res.render("homepage", {
       blogs,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
